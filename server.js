70778914ffede3053270b2dd0446f83f4a8759a6
mgrsk <mgrivas777@gmail.com>
2021-02-21 14:06:42 -0800

    app.js

--- server.js
+++ server.js
@@ -2,19 +2,8 @@ import app from './app';
 import config from './config';
 
 const { PORT } = config;
-const detect = require('detect-port');
 
 const portNum = PORT || 5000;
 console.log("PORT NO: ");
 console.log(portNum);
-
-detect(portNum, (err, _port) => {
-    if (err) {
-        console.log(err);
-    }
-
-    if (portNum === _port) {
-        app.listen(portNum, () => console.log(`Server started on PORT ${portNum}`));
-    }
-});
-
+app.listen(portNum, () => console.log(`Server started on PORT ${portNum}`));
