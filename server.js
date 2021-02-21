1c951fef8aeccca89635baecce4b2bb82aca4610
mgrsk <mgrivas777@gmail.com>
2021-02-21 15:08:57 -0800

    reverted server/env code back to what it was

--- server.js
+++ server.js
@@ -3,7 +3,4 @@ import config from './config';
 
 const { PORT } = config;
 
-const portNum = PORT || 5000;
-console.log("PORT NO: ");
-console.log(portNum);
-app.listen(portNum, () => console.log(`Server started on PORT ${portNum}`));
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
