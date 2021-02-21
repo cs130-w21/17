c5b744032ff6af0ad9d384000c1bf07e43e31064
mgrsk <mgrivas777@gmail.com>
2021-02-21 15:10:31 -0800

    reverted app.js

--- app.js
+++ app.js
@@ -47,13 +47,10 @@ app.use('/api/calendar', calendarRoutes);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  console.log("using static assets");
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-} else {
-    console.log("not prod");
 }
 
 export default app;
