4e36779f8820c991f7ce04bdcdd5c743510f0826
Manuel Rivas <37969321+mgrsk@users.noreply.github.com>
2021-02-26 13:21:24 -0800

    Add more frontend tests (#47)
    * Fixed dependency issue that caused client tests to crash. Added basic tests to App/HomePage
    
    * updated compiler target to es6
    
    * Updated package.json so travis CI build won't crash
    
    * Added more unit tests for the frontend.

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -8,6 +7,6 @@ configure({ adapter: new Adapter() });
 
 describe('<App /> Test', () => {
     it('renders without errors', () => {
-        const app = render(<App/>);
+        const app = shallow(<App/>);
     });
 });
