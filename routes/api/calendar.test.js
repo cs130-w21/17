3bdf7785a7a955eee80e4978be87c118b1c325e2
mvttchan <68944637+mvttchan@users.noreply.github.com>
2021-03-04 18:52:09 -0800

    Final backend (#67)
    * store files
    
    * Sprint 3 story
    
    * Error with package.json
    
    * Fixed dependencies
    
    * Deleted calendar tests
    
    * Deleted that babel module
    
    * changed test user
    
    * Put test ID into an env variable
    
    * Last test files
    
    * Added comments
    
    * beforeEach
    
    * beforeAll
    
    * Travis debugging
    
    * More debugging
    
    * Even more debugging
    
    * I'm losing my mind
    
    * Removed some test cases
    
    * lol
    
    * test calendar delete
    
    * readded calendar test
    
    * Finalized
    
    Co-authored-by: Matthew <mvttchan@protonmail.com>
    Co-authored-by: Manuel Rivas <37969321+mgrsk@users.noreply.github.com>

---
+++ routes/api/calendar.test.js
@@ -0,0 +1,52 @@
+import app from '../../app';
+require('dotenv').config();
+const supertest = require("supertest");
+
+let user_data = {
+    token: null,
+    refreshToken: process.env.REFRESH_TOKEN
+}
+let event = {
+    id: 'ASDF',
+    location: 'Somewhere',
+    summary: 'Appointment',
+    description: 'Get to it',
+    start: {
+        dateTime: '2021-12-16T10:00:00.000-07:00'
+    },
+    end: {
+        dateTime: '2021-12-16T10:25:00.000-07:00'
+    }
+}
+/**
+ * @name beforeAll calender tests
+ * @route /api/auth/accessToken
+ * @desc This just gathers a valid access token to use for testing the calendar's functionality.
+ */
+beforeAll(async() => {
+    await supertest(app)
+        .post('/api/auth/accessToken')
+        .send(user_data)
+        .then((res) => {
+            user_data.token = res.body.accessToken;
+        });
+});
+/**
+ * @name Calendar tests
+ * @route /api/calendar/
+ * @desc Unit tests for routes in /api/calendar
+ */
+describe('Calendar functionality', () => {
+    /**
+     * @name Get calendar page
+     * @route {POST} /api/calendar/getcalendar
+     * @routeparam {request} Send a refresh token to see if the get calendar route works.
+     *      This is expected to succeed givenn a valid refresh token.
+     */
+    test("POST get calendar", async () => {
+        await supertest(app)
+            .post("/api/calendar/getcalendar")
+            .send(user_data)
+            .expect(200)
+    });
+});
